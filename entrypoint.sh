49b762fec99bcd563aa9042f65a06cfe0b1b3126
mobiotwin <malnakli@biotwin.ai>
2022-04-11 13:29:49 -0400

    use glob options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 echo "Creating archive";
 mkdir /out
 cd $GITHUB_WORKSPACE
-zip -r /out/repoarchive.zip ./* -x .git/*
-export FILE_PATH='/out/repoarchive.zip'
 
 node /app/index.js
 [ $? -eq 0 ]  || exit 1
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 var spsave = require("spsave").spsave;
-var fs = require('fs');
+// var fs = require("fs");
 
-function trimSlashes(string) {
-    return string.replace(new RegExp('/', 'g'), '_');
-}
+// function trimSlashes(string) {
+//   return string.replace(new RegExp("/", "g"), "_");
+// }
 
 var coreOptions = {
-    siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.SITE_URL,
 };
 var creds = {
-    username: process.env.USER,
-    password: process.env.PASSWD
+  username: process.env.USER,
+  password: process.env.PASSWD,
 };
 
-var now = new Date().toISOString().slice(0,10);
+// var now = new Date().toISOString().slice(0, 10);
 
 var ref = "";
 if (process.env.GITHUB_REF) {
-  ref = process.env.GITHUB_REF.substr(process.env.GITHUB_REF.lastIndexOf('/') + 1);
+  ref = process.env.GITHUB_REF.substr(
+    process.env.GITHUB_REF.lastIndexOf("/") + 1
+  );
 }
 
+// var fileOptions = {
+//     folder: process.env.LIB_FOLDER,
+//     fileName: `${trimSlashes(process.env.GITHUB_REPOSITORY)}_${ref}_${now}.zip`,
+//     fileContent: fs.readFileSync(process.env.FILE_PATH)
+// };
+
 var fileOptions = {
-    folder: process.env.LIB_FOLDER, 
-    fileName: `${trimSlashes(process.env.GITHUB_REPOSITORY)}_${ref}_${now}.zip`,
-    fileContent: fs.readFileSync(process.env.FILE_PATH)
+  folder: process.env.LIB_FOLDER,
+  glob: "**/*.pbix",
 };
 
 spsave(coreOptions, creds, fileOptions)
-.then(function(){
-    console.log('Success');
-})
-.catch(function(err){
+  .then(function () {
+    console.log("Success");
+  })
+  .catch(function (err) {
     process.exit(1);
-});
+  });
